8981577a39bdf494e62f57ae8a20b29b87158149
ortex <diskbu@yandex.ru>
2016-05-26 00:03:17 +0300

    finished 2.60

--- chapter2/ex2.59-60.scm
+++ chapter2/ex2.59-60.scm
@@ -28,11 +28,36 @@
 (define (union-set set1 set2)
   (cond ((null? set1) set2)
         ((null? set2) set1)
-        (else (union-set (adjoin-set (car set2) set1)
-                         (cdr set2)))))
+        (else (cons (car set1)
+                    (union-set (cdr set1) set2)))))
 
 (define set1 '(1 2 3 4))
 (define set2 '(4 5 6 7))
 
 (intersection-set set1 set2) ; (4)
 (union-set set1 set2) ; (7 6 5 1 2 3 4)
+
+; 2.60
+
+; element-of-set? - without change
+
+(define (adjoin-set x set) (cons x set))
+
+(define (union-set set1 set2) (append set1 set2))
+
+(define (intersection-set set1 set2)
+  (define (intersection-iter set result)
+    (cond ((null? set) result)
+          ((and (element-of-set? (car set) set2)
+                (not (element-of-set? (car set) result)))
+            (intersection-iter (cdr set) (cons (car set) result)))
+          (else (intersection-iter (cdr set) result))))
+  (intersection-iter set1 '()))
+
+(define set1 '(1 2 3 4 4 4 4))
+(define set2 '(4 5 6 7))
+
+(intersection-set set1 set2) ; (4)
+
+; old union-set O(n^2);        new union-set O(1)
+; old intersection-set O(n^2); new intersection-set O(n^3)
