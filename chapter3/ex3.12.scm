500d0dfc766f81e01d89ad54b76b148121f08f65
ortex <diskbu@yandex.ru>
2016-08-21 19:33:55 +0300

    finidh 3.12

---
+++ chapter3/ex3.12.scm
@@ -0,0 +1,19 @@
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+
+(define z (append x y))
+
+(cdr x) ; (b)
+
+(define w (append! x y))
+
+(cdr x) ; (b c d)
