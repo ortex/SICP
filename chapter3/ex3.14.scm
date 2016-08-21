00dbe258c3066bd3c12e9029507538a4144b45e0
ortex <diskbu@yandex.ru>
2016-08-21 19:48:12 +0300

    finidh 3.14

---
+++ chapter3/ex3.14.scm
@@ -0,0 +1,17 @@
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+
+(define v '('a 'b 'c 'd))
+
+(define w (mystery v))
+
+v ; ('a)
+
+w ; ('d 'c 'b 'a)
