47a9cef9b72615c3d226aaa3dc1395aee8a7d56b
ortex <diskbu@yandex.ru>
2016-09-01 22:45:35 +0300

    fix 3.18

--- chapter3/ex3.18.scm
+++ chapter3/ex3.18.scm
@@ -1,6 +1,6 @@
 (load "chapter3/ex3.13.scm")
 
-(define (exist-infinity-loop l)
+(define (exist-infinity-loop? l)
   (define (iter l elems)
     (if (null? l)
         false
@@ -9,8 +9,8 @@
             (iter (cdr l) (append elems (list (car l)))))))
   (iter l '()))
 
-(exist-infinity-loop '(1 2 23))
+(exist-infinity-loop? '(1 2 23)) ; false
 
 (define inf-loop (make-cycle '('a 'b 'c)))
 
-(exist-infinity-loop inf-loop)
+(exist-infinity-loop? inf-loop) ; true
