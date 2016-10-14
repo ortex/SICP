8e0751fda02cdfed12b0a94bb00474e18a9a2852
ortex <diskbu@yandex.ru>
2016-10-14 23:52:38 +0300

    finished 3.52

---
+++ chapter3/ex3.52.scm
@@ -0,0 +1,25 @@
+(define sum 0)
+
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+
+(define y (stream-filter even? seq))
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0))
+                         seq))
+
+(stream-ref y 7)
+;; ;Value: 136
+
+(display-stream z)
+
+;; 10
+;; 15
+;; 45
+;; 55
+;; 105
+;; 120
+;; 190
+;; 210
