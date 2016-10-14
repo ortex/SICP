91bffd426bfcbd8e58207c7466d2a4ef308f87ab
ortex <diskbu@yandex.ru>
2016-10-14 23:44:44 +0300

    finished 3.51

---
+++ chapter3/ex3.51.scm
@@ -0,0 +1,15 @@
+(define (show x)
+  (display x)
+  x)
+
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+;; 0
+;; ;Value: x
+
+(stream-ref x 5)
+;; 12345
+;; ;Value: 5
+
+(stream-ref x 7)
+;; 67
+;; ;Value: 7
