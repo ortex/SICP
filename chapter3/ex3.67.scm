d815231c657b9afe14b309821de90c874a554d0a
ortex <diskbu@yandex.ru>
2016-10-29 16:41:11 +0300

    finished 3.67

---
+++ chapter3/ex3.67.scm
@@ -0,0 +1,20 @@
+(load "chapter3/streams.scm")
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+    (interleave
+      (interleave
+        (stream-map (lambda (x) (list (stream-car s) x))
+                    (stream-cdr t))
+        (stream-map (lambda (x) (list x (stream-car t)))
+                    (stream-cdr s)))
+      (pairs (stream-cdr s) (stream-cdr t)))))
+
+(displayln-first-n (pairs integers integers) 100)
